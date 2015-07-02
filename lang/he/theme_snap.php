e8994e884f1a5ca3ac6a15c5a186fd70602e5433
Nadav Kavalerchik <nadavkav@gmail.com>
2015-07-02 21:44:59 +0300

    Added (initial) Hebrew translation

---
+++ lang/he/theme_snap.php
@@ -0,0 +1,160 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package   theme_snap
+ * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ */
+
+$string['aboutcourse'] = 'אודות קורס זה';
+$string['activity'] = 'פעילות';
+$string['addanewsection'] = 'הוספת יחידת הוראה חדשה';
+$string['admin'] = 'ניהול';
+$string['advancedbrandingheading'] = 'Advanced branding';
+$string['advancedbrandingheadingdesc'] = '';
+$string['ago'] = 'ago';
+$string['alerts'] = 'התראות';
+$string['answered'] = 'Answered';
+$string['appendices'] = 'כלים';
+$string['assigndraft'] = 'Draft requires your confirmation';
+$string['assignreopened'] = 'Reopened';
+$string['at'] = 'ב';
+$string['attempted'] = 'Attempted';
+$string['brandingheading'] = 'Branding';
+$string['brandingheadingdesc'] = '';
+$string['browseallcourses'] = 'צפיה בכל הקורסים';
+$string['changecourseimage'] = 'Change course image';
+$string['changecoverimage'] = 'Change cover image';
+$string['changefullname'] = 'Change site name';
+$string['chapters'] = 'נושאים';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>Snap</h2><img class=img-polaroid src="snap/pix/screenshot.jpg" />';
+$string['close'] = 'סיום';
+$string['conditional'] = 'Conditional';
+$string['contents'] = 'יחידות הוראה';
+$string['contributed'] = 'Contributed';
+$string['coursecontacts'] = 'משתתפי הקורס';
+$string['coursefootertoggle'] = 'Show Course Footer';
+$string['coursefootertoggledesc'] = 'The Course Footer shows useful information to users on a course page incuding course contacts, course description and recent activity in the course.';
+$string['coursenavigation'] = 'ניווט בקורס';
+$string['coursetools'] = 'כלי הקורס';
+$string['createsection'] = 'יצירת יחידת לימוד חדשה';
+$string['current'] = 'יחידת הוראה נוכחית';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Add any extra CSS for your site.';
+$string['deadlines'] = 'מועדי הגשה';
+$string['deadlinestoggle'] = 'Show Deadlines in personal menu';
+$string['deadlinestoggledesc'] = 'Deadlines show any upcoming activity deadlines from courses you are enrolled on.';
+$string['debugerrors'] = 'Debug Errors';
+$string['draft'] = 'טיוטה';
+$string['due'] = 'Due';
+$string['editcoursecontent'] = 'Edit course content';
+$string['editcoursesettings'] = 'Edit course settings';
+$string['editcustomfooter'] = 'Edit footer';
+$string['editcustommenu'] = 'Edit custom menu';
+$string['favicon'] = 'Favicon';
+$string['favicondesc'] = 'Please upload your favicon in .ico format here. The favicon will be displayed along with the page title in your browser’s tab bar and in bookmarks.';
+$string['feedbackavailable'] = 'משוב וציונים';
+$string['feedbacktoggle'] = 'Show Recent Feedback & Ungraded in personal menu';
+$string['feedbacktoggledesc'] = 'Recent Feedback shows recent feedback to learners and submissions which need grading to educators.';
+$string['footnote'] = 'Site footer';
+$string['footnotedesc'] = 'A footer for your site. This is the best place to put links to help, support and any other sites your organisation has that you want learners/educators to be aware of e.g. library, email etc.';
+$string['forumauthor'] = 'Author';
+$string['forumlastpost'] = 'Last Post';
+$string['forumpicturegroup'] = 'Group';
+$string['forumreplies'] = 'Replies';
+$string['forumtopic'] = 'Topic';
+$string['fullname'] = 'Site name';
+$string['fullnamedesc'] = 'The name of your site.';
+$string['graderadviseuserreport'] = 'The "grader report" does not work well on mobile devices. The "user report" is recommended instead';
+$string['grading'] = 'בדיקת מטלות';
+$string['headingfont'] = 'Heading font';
+$string['headingfont_desc'] = 'This font is used in the headings (h1 - h6 elements) across your site. If you are including a custom webfont remember to add it to the moodle extra html form.';
+$string['help'] = 'Help';
+$string['helpwithlogin'] = 'Help with login';
+$string['helpwithloginandguest'] = 'Help with login / guest access';
+$string['hidenavblock'] = 'Hide default Moodle navigation block';
+$string['hidenavblockdesc'] = "In Snap we recommend hiding the default Moodle navigation block. ";
+$string['highlightedsection'] = 'highlighted';
+$string['home'] = 'home';
+$string['image'] = 'image';
+$string['introduction'] = 'Introduction';
+$string['knowledgebase'] = 'Moodlerooms Knowledge Base';
+$string['loading'] = 'Loading...';
+$string['loggedinasguest'] = 'You are logged in as guest';
+$string['loginform'] = 'Login form';
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here. The logo will be displayed in the header across the entire site.';
+$string['manuals'] = 'חוברות הדרכה';
+$string['menu'] = 'תפריט';
+$string['menusandnavheading'] = 'תפריט וניווט';
+$string['menusandnavheadingdesc'] = '';
+$string['messageread'] = 'Message Read';
+$string['messages'] = 'הודעות';
+$string['messagestoggle'] = 'Show Messages in personal menu';
+$string['messagestoggledesc'] = 'Messages shows the most recent messages from other users.';
+$string['messaging'] = 'מסרים והודעות';
+$string['more'] = 'עוד';
+$string['morenews'] = 'עוד חדשות';
+$string['nextsection'] = 'ליחידה הבאה';
+$string['nodeadlines'] = 'לא נמצאו מועדי הגשת מטלות להם נדרשת התייחסות.';
+$string['nograded'] = 'לא התקבל משוב חדש מהמורים.';
+$string['nograding'] = 'לא ממתינות הגשות תלמידים אותן נדרש לבדוק.';
+$string['nomessages'] = 'לא נמצאו הודעות חדשות.';
+$string['notanswered'] = 'טרם הוגש';
+$string['notattempted'] = 'טרם החל ניסיון מענה';
+$string['notcontributed'] = 'טרם נתרם';
+$string['notpublished'] = 'לא זמין לתלמידים';
+$string['notsubmitted'] = 'טרם הוגש';
+$string['overdue'] = 'Overdue';
+$string['pld'] = 'PLD';
+$string['pluginname'] = 'Snap';
+$string['poster'] = 'Cover image';
+$string['posterdesc'] = 'A large image for the front page of your site. The image should exclude text. Images of 1280px width by 500px height or larger (up to 2000px in width) work best. Optimising your image for the web will speed up the page load time.';
+$string['poweredbyrunby'] = '<a href="http://www.moodlerooms.com/meet-joule-lms" target="_blank"><img class="poweredbylogo" src="{$a}" alt="joule"/></a> powered by';
+$string['previoussection'] = 'ליחידה הקודמת';
+$string['problemsfound'] = 'נמצאו בעיות, להן נדרשת התייחסות';
+$string['readmore'] = 'מידע נוסף&nbsp;»';
+$string['recentactivity'] = 'פעילות אחרונה';
+$string['recentfeedback'] = 'משוב אחרון';
+$string['region-main'] = 'ראשי';
+$string['region-side-post'] = 'ימין';
+$string['region-side-pre'] = 'שמאל';
+$string['region-side-top'] = 'בראש העמוד';
+$string['released'] = 'Released: {$a}';
+$string['reopened'] = 'Reopened';
+$string['removethissection'] = 'הסרת יחידת הוראה זו';
+$string['seriffont'] = 'Serif font';
+$string['seriffont_desc'] = 'This font is used for most user generated content. Using a serif font makes user generated content more readable, and feel like it is writen by a human.';
+$string['sitenavigation'] = 'site navigation';
+$string['submitted'] = 'הוגשו';
+$string['subtitle'] = 'תאור המערכת';
+$string['subtitle_desc'] = 'A brief tag line about your site, about 240 characters in length. Your site description will be displayed on the site front page.';
+$string['themecolor'] = 'Brand color';
+$string['themecolordesc'] = 'The primary color for your site which is used in links, buttons and menu backgrounds. Bright colours work best and will give your site a modern feel.';
+$string['togglenavigation'] = 'Toggle navigation';
+$string['unread'] = 'טרם נקרא';
+$string['via'] = 'בעזרת';
+$string['viewcourse'] = 'צפיה בקורס';
+$string['viewyourprofile'] = 'צפיה בפרופיל האישי';
+$string['warnsiteformatflexpage'] = 'Your site uses Flexpage on the front page. Flexpage is not optimized for small screen devices. It is recommended that you disable the use of Flexpage in your <a href="{$a}">front page settings</a> if you would like your site to be mobile device friendly.';
+$string['xofyanswered'] = '{$a->completed} מתוך {$a->participants} ענו';
+$string['xofyattempted'] = '{$a->completed} מתוך {$a->participants} התנסו';
+$string['xofycontributed'] = '{$a->completed} מתוך {$a->participants} תרמו';
+$string['xofysubmitted'] = '{$a->completed} מתוך {$a->participants} הגישו';
+$string['xungraded'] = '{$a} טרם נבדקו';
